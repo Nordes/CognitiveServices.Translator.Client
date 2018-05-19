eaf099cd7c2f56999ff76b38eadcee0f57305a43
Nordes Lamarre <nordes@gmail.com>
2018-05-20 01:09:18 +0900

    Change some text in the home page and add image

--- CognitiveServices.Translator.Wrapper.WebSample/ClientApp/components/home-page.vue
+++ CognitiveServices.Translator.Wrapper.WebSample/ClientApp/components/home-page.vue
@@ -2,12 +2,31 @@
     <div>
         <h1>Cognitive Services Translator Wrapper Demo!</h1>
         <p>More details can be found on the Github page of the project at: <a href="https://github.com/Nordes/CognitiveServices.Translator.Wrapper">https://github.com/Nordes/CognitiveServices.Translator.Wrapper</a></p>
-
+        <p>
+          This is the demo using the nuget package "CognitiveServices.Translator.Wrapper" available on Nuget.
+        </p>
+        
+        <div class="row w-75">
+          <div class="col-md-2 text-right">
+            <img height="60px" :src="require('../assets/img/nuget.png')"/>
+          </div>
+          <div class="col-md-10 nblue pt-3">
+            PM&gt; Install-Package CognitiveServices.Translator.Wrapper -Version 1.0.0
+          </div>
+        </div>
         <br><br>
-        <p>Contact me via <a class="h3" href="https://twitter.com/nordes"><icon :icon="['fab', 'twitter-square']"/></a> or
-         <a class="h3" href="https://www.linkedin.com/in/nordes"><icon :icon="['fab', 'linkedin']"/></a></p>
+        <p>
+          My name is Nordès and you can contact me using <a class="h4" href="https://twitter.com/nordes">Twitter<icon :icon="['fab', 'twitter-square']"/></a> or
+         <a class="h4" href="https://www.linkedin.com/in/nordes">Linked<icon :icon="['fab', 'linkedin']"/></a></p>
     </div>
 </template>
+
+<style scoped>
+  .nblue {
+    background-color: #002440;
+    color: #fff;
+  }
+</style>
 
 <script>
 export default {
@@ -16,6 +35,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
