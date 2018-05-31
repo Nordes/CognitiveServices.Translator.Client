7ecc074b6dfc583679889ee50147dc7a8ce41f76
Nordes Lamarre <nordes@gmail.com>
2018-05-31 22:01:03 +0900

    Change dotnet publish verbosity

--- createArtifacts.ps1
+++ createArtifacts.ps1
@@ -5,7 +5,7 @@ $artifacts = "$((Get-Location).Path)/artifacts"
 # Remove all the files from the nupkgs folder
 # Remove-Item "$artifacts/*" -Force -Recurse 
 
-$cmd = "dotnet publish ./CognitiveServices.Translator.Client.WebSample/CognitiveServices.Translator.Client.WebSample.csproj -c=Release -o=$artifacts"
+$cmd = "dotnet publish ./CognitiveServices.Translator.Client.WebSample/CognitiveServices.Translator.Client.WebSample.csproj -c=Release -v q -o=$artifacts"
 Invoke-Expression $cmd
 
 Compress-Archive -Path "$artifacts/*" -DestinationPath "$artifacts\demoWebsite.zip"
